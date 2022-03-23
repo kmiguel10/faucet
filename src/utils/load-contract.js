c928a759929732401e2dda6499fb2ecebd89bd3a
Kent Miguel <kentmiguel10@gmail.com>
2022-03-23 08:16:14 -0400

    added comments

--- src/utils/load-contract.js
+++ src/utils/load-contract.js
@@ -4,8 +4,8 @@ import contract from "@truffle/contract";
 //Receives contract name
 export const loadContract = async (name) => {
   //access json file from public folder - makes request to our localhost
-  const res = await fetch(`/contracts/${name}.json`);
-  const Artifact = await res.json(); //extract json artifact file
+  const res = await fetch(`/contracts/${name}.json`); //this is why we changed the structure of the artifcats/build to be in public folder
+  const artifact = await res.json(); //extract json artifact file
 
-  return contract(Artifact); //wrap artifact in contract abstraction
+  return contract(artifact); //wrap artifact in contract abstraction
 };
