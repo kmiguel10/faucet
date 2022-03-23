9838c9897c94b84f0d9f75800af3c960109229c8
Kent Miguel <kentmiguel10@gmail.com>
2022-03-22 20:18:03 -0400

    integrate bulma
    1. Integrated bulma framework for css styling

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return <div className="App">Hello World!</div>;
+  return (
+    <div className="faucet-wrapper">
+      <div className="faucet">
+        <div className="balance-view is-size-2">
+          Current Balance: <strong>10</strong> ETH
+        </div>
+        <button className="btn mr-2">Donate</button>
+        <button classNmae="btn">Withdraw</button>
+      </div>
+    </div>
+  );
 }
 
 export default App;
